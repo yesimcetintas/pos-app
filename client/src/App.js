47b76da15d3ede0fd1bc63e9f452a19796f2d4e2
yesimcetintas <yesimturker@outlook.com>
2023-04-27 16:44:19 +0300

    Categories & Products Components add

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import Categories from './components/categories/Categories';
 import Header from './components/header/Header';
+import Products from './components/products/Products';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className='home flex justify-between px-6 gap-10'>
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)]">
+          <Categories/>
+        </div>
+        <div className="products flex-[8]">
+          <Products/>
+        </div>
+        <div>
+          <div>cart totals</div>
+        </div>
+      </div>
     </div>
   );
 }
