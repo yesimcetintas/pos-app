4431e793ba03deeb9f00d152df32df78636a26d0
yesimcetintas <yesimturker@outlook.com>
2023-04-28 16:29:55 +0300

    Cart Component Add & Home Page Responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import CartTotals from './components/cart/CartTotals';
 import Categories from './components/categories/Categories';
 import Header from './components/header/Header';
 import Products from './components/products/Products';
@@ -6,15 +7,15 @@ function App() {
   return (
     <div>
       <Header/>
-      <div className='home flex justify-between px-6 gap-10'>
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)]">
+      <div className='home flex md:flex-row flex-col justify-between px-6 gap-10 md:pb-0 pb-24'>
+        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
           <Categories/>
         </div>
-        <div className="products flex-[8]">
+        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
           <Products/>
         </div>
-        <div>
-          <div>cart totals</div>
+        <div className='cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border'>
+          <CartTotals/>
         </div>
       </div>
     </div>
--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 
 const Categories = () => {
   return (
-    <ul className='flex gap-4 flex-col text-lg'>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+    <ul className='flex gap-4 text-lg md:flex-col'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>Tümü</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>Yiyecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
     </ul>
